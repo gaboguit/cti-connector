5d8f96c999adb390644d6225451e42777538a29c
alexcomo95 <alexandrecomeau95@gmail.com>
2021-09-22 23:20:42 -0400

    Customer data added to the rows

--- examples/js/action-links.js
+++ examples/js/action-links.js
@@ -1,43 +1,38 @@
 
 var connector;
+var pos;
 
 var onLabelSet = function onLabelSet(id) {
     var me = this;
     var client = me.calls[id].id;
     alert(client);
-
 };
 
 Cti.Platform = function () {
     // some initialization
     Cti.log("Loading connector");
     var me = this;
-
     $('#status').html("Loading connector...");
     $('#status')
             .removeClass()
             .addClass('label label-warning');
-
     connector = new Cti.Connector({
         // callback
-        
         onMessage: function(message) {
             me.onMessage(message);
         }
     });
+    pos = new Pos.Service();
 };
 
 Cti.Platform.prototype = {
     username: null,
-    calls: {
-
-    },
+    calls: {},
     callEvents: [
         Cti.EVENT.INITIAL, Cti.EVENT.ACCEPTED, Cti.EVENT.RINGING, Cti.EVENT.CONNECTED, Cti.EVENT.ON_HOLD, Cti.EVENT.HANGUP, Cti.EVENT.CANCEL
     ],
     run: function () {
         if (!connector.isConnected()) {
-
             document.title = "Ready";
             $('#status')
                     .html("Platform ready: Not connected.")
@@ -51,166 +46,166 @@ Cti.Platform.prototype = {
             .html("Authentication " + username + "...")
             .removeClass()
             .addClass('label label-warning');
-
         this.username = username;
-
         connector.login.apply(connector, arguments);
     },
     logout: function () {
         this.username = null;
-
         connector.logout();
+    },
+    assignCustomer: function(call, customer) {
+        var me = this;
+        if (customer) {
+            me.calls[call.id].firstname = customer.firstname;
+            me.calls[call.id].lastname = customer.lastname;
+            me.calls[call.id].email = customer.email;
+            me.calls[call.id].note = customer.internalNote;
+        }
+        me.calls[call.id].loadingCustomer = false;
+        me.refreshDisplay(call);
     },
     onCallEvent: function(call) {
         var me = this;
-        if (call.status == "HANGUP" ) {     
-            if (me.calls[call.id]) { 
+        if (call.status == "RINGING") {
+            if (me.calls[call.id]) {
+                me.calls[call.id].loadingCustomer = true;
+                var parsedPhone = call.source.slice(0,3) + '-' + call.source.slice(3);
+                var parsedPhone = parsedPhone.slice(0,7) + '-' + parsedPhone.slice(7);
+                var bindedCallback = me.assignCustomer.bind(me);
+                pos.getCustomer(parsedPhone, call, bindedCallback);
+                me.refreshDisplay(call);
+            }
+        }
+        if (call.status == "HANGUP" ) {
+            if (me.calls[call.id]) {
                 me.calls[call.id].status = call.status;
+                // Q: What is the purpose of this variable?
                 callStatus = call.status;
                 console.log(me.calls);
-                refreshDisplay(); 
-                clearInterval(refreshTimer);   
-                setTimeout(function(){
+                me.refreshDisplay(call);
+                clearInterval(refreshTimer);
+                setTimeout(function() {
                     delete(me.calls[call.id]);
                     $('#call-' + call.id).remove();
-                },2000);         
+                }, 2000);
             }
         } else {
             if (!me.calls[call.id]) {
                 me.calls[call.id] = call;
-                
             }
         }
         if (call.status == "ON_HOLD" ) {
             me.calls[call.id].status = call.status;
+            // Q: What is the purpose of this variable?
             callStatus = call.status;
             console.log(me.calls);
-            
         }
         if (call.status == "CONNECTED" ) {
             me.calls[call.id].status = call.status;
+            // Q: What is the purpose of this variable?
             callStatus = call.status;
             console.log(me.calls);
-            var refreshTimer = setInterval(function(){ 
-                refreshDisplay();   
+            // Q: So there is a timer refreshing the display for each call event?
+            var refreshTimer = setInterval(function() {
+                me.refreshDisplay(call);
             }, 3000);
         }
-        
-        
-        function refreshDisplay (){
-            for (callId in me.calls) {
-                thisCall = me.calls[callId];
-                var row = '<tr id="call-'+callId+'">';
-                row+= '<td>'+thisCall.status+'</td>';
-                row+= '<td>'+thisCall.source+'</td>';
-                row+= '<td>'+thisCall.destination+'</td>';
-                row+= '<td>'+thisCall.destinationName+'</td>';
-                row+= '<td><input type="text" name="prenom" class="form-control" placeholder="prenom" /></td>';
-                row+= '<td><input type="text" name="nom" class="form-control" placeholder="nom" /></td>';
-                row+= '<td><input type="text" name="email" class="form-control" placeholder="email" /></td>';
-                row+= '<td><input type="text" name="note" class="form-control" placeholder="note" /></td>';
-                row+= '<td><a href="#" onclick="" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-right"></span> Set</a></td>';
-                row+= '</tr>';
-                if ($('#call-' + callId).length) {
-                    $('#call-' + call.id).replaceWith(row);
-                    console.log('replaced');
-                } else{
-                    $('#calls-table tr:last').after(row);
-                }
-                
-    
+    },
+    refreshDisplay: function(call) {
+        var me = this;
+        for (callId in me.calls) {
+            thisCall = me.calls[callId];
+            var row = '<tr id="call-' + callId + '">';
+            row += '<td>' + thisCall.status + '</td>';
+            row += '<td>' + thisCall.source + '</td>';
+            row += '<td>' + thisCall.destination + '</td>';
+            row += '<td>' + thisCall.destinationName + '</td>';
+            console.log(thisCall.loadingCustomer);
+            if (thisCall.loadingCustomer) {
+                row += '<td><div class="boxLoading"></div></td>';
+                row += '<td><div class="boxLoading"></div></td>';
+                row += '<td><div class="boxLoading"></div></td>';
+                row += '<td><div class="boxLoading"></div></td>';
+            } else {
+                row += '<td><input type="text" name="prenom" class="form-control" placeholder="prenom" value=' + thisCall.firstname + ' /></td>';
+                row += '<td><input type="text" name="nom" class="form-control" placeholder="nom" value=' + thisCall.lastname + ' /></td>';
+                row += '<td><input type="text" name="email" class="form-control" placeholder="email" value=' + thisCall.email + ' /></td>';
+                row += '<td><input type="text" name="note" class="form-control" placeholder="note" value=' + thisCall.note + ' /></td>';
+            }
+            row += '<td><a href="#" onclick="" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-right"></span> Set</a></td>';
+            row += '</tr>';
+            // Q: So all existing calls are replaced with the current call?
+            if ($('#call-' + callId).length) {
+                $('#call-' + call.id).replaceWith(row);
+                console.log('replaced');
+            } else {
+                $('#calls-table tr:last').after(row);
             }
         }
-        
-        
     },
     onMessage: function (event) {
-
         var me = this;
-
         Cti.log(event);
-
         if (event.name === Cti.EVENT.READY) {
             document.title = "Connected";
-
             $('#status')
                     .html("Connected")
                     .removeClass()
                     .addClass('label label-success');
-
             $('#disconnect').show();
             $('#login-form').hide();
-
             if (me.bootstrap) {
                 me.bootstrap();
             }
         }
-
-
         if ($.inArray(event.name, me.callEvents) !== -1) {
             me.onCallEvent(event.call);
         }
-
         if ($.inArray(event.name, [Cti.EVENT.ERROR, Cti.EVENT.INFO]) !== -1) {
             document.title = "Error";
-
             $('#status')
                     .html("Error")
                     .removeClass()
                     .addClass('label label-danger');
-
             alert(event.message);
         }
-
         if (event.name === Cti.EVENT.LOGGED_IN) {
             // add code if needed
         }
-
         if (event.name === Cti.EVENT.LOGGED_OUT) {
             document.title = "Ready";
-
             $('#status')
                     .html("Platform ready: Not connected.")
                     .removeClass()
                     .addClass('label label-info');
-
             $('#login-form').show();
             $('#disconnect').hide();
-
         }
     }
 };
 
 $().ready(function () {
-
     var platform = new Cti.Platform();
-
     platform.bootstrap = function() {
         Cti.log('Subscribing to all user events');
         var myUserId = connector._getParam('user_id');
-
         connector.apiRequest('GET', '/users', null, function(json) {
             json.data.forEach(function(user) {
                 if (user.id == myUserId) {
                     return;
                 }
-
                 connector.subscribe('user:' + user.id);
             });
         });
     };
     // run platform
     platform.run();
-
-
     $('#connect-with-username-password').click(function() {
         platform.login($('#username').val(), $('#password').val());
     });
-    
     $('#connect-with-api-key').click(function() {
         platform.login($('#api_key').val());
     });
-    
     $('#disconnect').click(function() {
         platform.logout();
     });
---
+++ examples/js/pos.js
@@ -0,0 +1,132 @@
+Pos = {};
+
+Pos.Service = function() {
+    var me = this;
+}
+
+Pos.Service.prototype = {
+    _request: function(config) {
+        var me = this;
+        var xhr = me._newXHR();
+        var endpoint = "https://md.phppointofsale.com/index.php/api/v1/";
+        xhr.open(config.method, endpoint + config.url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        if (config.key) {
+            xhr.setRequestHeader("x-api-key", config.key);
+        }
+        if (config.data) {
+            xhr.setRequestHeader("Content-length", JSON.stringify(config.data));
+        }
+        xhr.setRequestHeader("accept", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (xhr.status === 0) {
+                config.failure(xhr.status, { message: 'PoS Connection failure', errors: [] });
+                return;
+            }
+            var response;
+            if (xhr.responseText) {
+                try {
+                    response = JSON.parse(xhr.responseText);
+                } catch (err) {
+                    config.failure(xhr.status, { message: 'Failed to parse PoS response', errors: [] });
+                    return;
+                }
+            }
+            if (xhr.status >= 200 && xhr.status < 400) {
+                config.success(response);
+            } else {
+                config.failure(xhr.status, response);
+            }
+        };
+        if (config.data) {
+            return xhr.send(JSON.stringify(config.data));
+        } else {
+            return xhr.send();
+        }
+    },
+    _newXHR: function () {
+        var xhr = null;
+        if (window.XDomainRequest) {
+            console.log("using XdomainRequest for IE");
+            var fireReadyStateChange = function (xhr, status) {
+                xhr.status = status;
+                xhr.readyState = 4;
+                try {
+                    xhr.onreadystatechange();
+                } catch(e) {
+                }
+            };
+            xhr = new XDomainRequest();
+            xhr.readyState = 0;
+            xhr.onload = function () {
+                var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+                xmlDoc.async = "false";
+                xmlDoc.loadXML(xhr.responseText);
+                xhr.responseXML = xmlDoc;
+                fireReadyStateChange(xhr, 200);
+            };
+            xhr.onerror = function () {
+                fireReadyStateChange(xhr, 500);
+            };
+            xhr.ontimeout = function () {
+                fireReadyStateChange(xhr, 500);
+            };
+        } else if (window.XMLHttpRequest) {
+            console.log("using XMLHttpRequest");
+            xhr = new XMLHttpRequest();
+            if (xhr.overrideMimeType) {
+                xhr.overrideMimeType("text/xml; charset=utf-8");
+            }
+        } else if (window.ActiveXObject) {
+            console.log("using ActiveXObject");
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        return xhr;
+    },
+    getCustomer: function(phone, call, callback) {
+        var me = this;
+        var data = {
+            method: 'GET',
+            url: '/customers?limit=1&search_field=phone_number&search=' + phone,
+            key: "8g0c8cooc0804occgscg484g00oks8048k0ok08w",
+            success: function(response) {
+                var customer = new Pos.Customer(response[0]);
+                console.log(customer);
+                callback(call, customer);
+            },
+            failure: function() {
+                console.log("Failed to retrieve the customer's information.");
+                callback(call, null);
+            }
+        };
+        me._request(data);
+    }
+}
+
+Pos.Customer = function(data) {
+    this.personId = data.person_id;
+    this.firstname = data.first_name;
+    this.lastname = data.last_name;
+    this.email = data.email;
+    this.phone = data.phone_number;
+    this.address_1 = data.address_1;
+    this.address_2 = data.address_2;
+    this.city = data.city;
+    this.zip = data.zip;
+    this.country = data.country;
+    this.comments = data.comments;
+    this.company = data.company_name;
+    this.accountNumber = data.account_number;
+    this.internalNote = data.internal_notes;
+    this.balance = data.balance;
+    this.creditLimit = data.credit_limit;
+    this.points = data.points;
+    this.disableLoyalty = data.disable_loyalty;
+    this.amountToSpendForNextPoint = data.amount_to_spend_for_next_point;
+    this.remainingSalesBeforeDiscount = data.remaining_sales_before_discount;
+}
+
+Pos.Customer.prototype.constructor = Pos.Customer;
